b1b75c510d8c25e2c30a50c1a2433cab1d92e589
tuphan-dn <tuphan@descartes.network>
2023-02-28 22:26:10 +0700

    support more solana mainnet rpc

--- src/list/solana-mainnet.ts
+++ src/list/solana-mainnet.ts
@@ -10,7 +10,10 @@ export class SolanaMainnet extends Chain {
       cryptosys: CryptoSys.EdDSA,
       icon: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/sol-icon.svg',
       logo: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/sol-icon.svg',
-      rpcs: ['https://api.mainnet-beta.solana.com'],
+      rpcs: [
+        'https://radial-billowing-gas.solana-mainnet.quiknode.pro/9d19e80c758eb5bd7b86d912e9345aa153db6a8f/',
+        'https://api.mainnet-beta.solana.com',
+      ],
       group: 'solana',
     })
   }
