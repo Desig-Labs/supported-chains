6634c7aa1079441229535a8dda7fe088652c6b1c
tuphan-dn <tuphan@descartes.network>
2023-05-08 22:53:54 +0700

    bugfix

--- genchain.ts
+++ genchain.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import { rimrafSync } from 'rimraf'
 import { sha512 } from '@noble/hashes/sha512'
 import BN from 'bn.js'
-import { Code } from './src/code'
+import { Curve } from './src/curve'
 
 const customizeChainId = (name: string) => {
   const r = BN.red(new BN(Number.MAX_SAFE_INTEGER))
@@ -10,46 +10,46 @@ const customizeChainId = (name: string) => {
   return seed.toRed(r).toNumber()
 }
 
-const chainIds: Array<{ name: string; id: number; code: Code }> = [
+const chainIds: Array<{ name: string; id: number; curve: Curve }> = [
   {
     name: 'Ethereum Mainnet',
     id: 1,
-    code: Code.secp256k1,
+    curve: Curve.secp256k1,
   },
   {
     name: 'Goerli',
     id: 5,
-    code: Code.secp256k1,
+    curve: Curve.secp256k1,
   },
   {
     name: 'Sepolia',
     id: 11155111,
-    code: Code.secp256k1,
+    curve: Curve.secp256k1,
   },
   {
     name: 'Binance Smart Chain Mainnet',
     id: 56,
-    code: Code.secp256k1,
+    curve: Curve.secp256k1,
   },
   {
     name: 'Binance Smart Chain Testnet',
     id: 97,
-    code: Code.secp256k1,
+    curve: Curve.secp256k1,
   },
   {
     name: 'Solana Mainnet',
     id: customizeChainId('Solana Mainnet'),
-    code: Code.ed25519,
+    curve: Curve.ed25519,
   },
   {
     name: 'Solana Testnet',
     id: customizeChainId('Solana Testnet'),
-    code: Code.ed25519,
+    curve: Curve.ed25519,
   },
   {
     name: 'Solana Devnet',
     id: customizeChainId('Solana Devnet'),
-    code: Code.ed25519,
+    curve: Curve.ed25519,
   },
 ]
 
@@ -61,11 +61,11 @@ fs.appendFileSync(
   '| # | Name | Elliptic Curve | Chain ID | Hex |\n',
 )
 fs.appendFileSync('./index.md', '| - | - | - | - | - |\n')
-chainIds.forEach(({ name, id, code }, i) => {
+chainIds.forEach(({ name, id, curve }, i) => {
   const hex = new BN(id).toArrayLike(Buffer, 'be').toString('hex')
   fs.appendFileSync(
     './index.md',
-    `| ${i} | ${name} | ${code} | ${id} | 0x${hex.replace(/^0+/, '')} |\n`,
+    `| ${i} | ${name} | ${curve} | ${id} | 0x${hex.replace(/^0+/, '')} |\n`,
   )
   console.log(`✅ ${name} - ${id}: 0x${hex.replace(/^0+/, '')}`)
 })
