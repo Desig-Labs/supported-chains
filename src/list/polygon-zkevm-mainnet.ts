f017021f39751fec9e5c58b11ab0702713e0117f
tuanle161099 <tuanle@descartes.network>
2023-07-06 13:26:38 +0700

    support polygon zkevm

---
+++ src/list/polygon-zkevm-mainnet.ts
@@ -0,0 +1,22 @@
+import { Chain } from '../chain'
+import { Curve } from '../curve'
+import { toEvmAddress } from '../utils'
+
+export class PolygonZkEVMMainNet extends Chain {
+  constructor() {
+    super({
+      name: 'Polygon zkEVM Mainnet',
+      alias: 'Polygon zkEVM',
+      chainId: '0x44d',
+      curve: Curve.secp256k1,
+      icon: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/polygon-icon.svg',
+      logo: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/polygon-logo.svg',
+      rpcs: [
+        'wss://rpc.ankr.com/polygon_zkevm/ws/6fd209c7de745965c5b2092cb14245501fb6d4bd12a11db785dda62ab94cb2a4',
+        'https://rpc.ankr.com/polygon_zkevm/6fd209c7de745965c5b2092cb14245501fb6d4bd12a11db785dda62ab94cb2a4',
+      ],
+      group: 'polygon_zkevm',
+      getAddress: toEvmAddress,
+    })
+  }
+}
