4d8be7481781aa6bf0a49b03d5bcc2df91477929
thiennv-qng <81727176+thiennv-qng@users.noreply.github.com>
2023-08-04 12:03:46 +0700

    shardeum chain

---
+++ src/list/shardeum-dapp.ts
@@ -0,0 +1,20 @@
+import { Chain } from '../chain'
+import { Curve } from '../curve'
+import { GroupChain } from '../types'
+import { toEvmAddress } from '../utils'
+
+export class ShardeumDapp extends Chain {
+  constructor() {
+    super({
+      name: 'Shardeum Sphinx Dapp 1.X',
+      alias: 'Shardeum Sphinx Dapp 1.X',
+      chainId: '8081',
+      curve: Curve.secp256k1,
+      icon: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/shardeum-icon.svg',
+      logo: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/shardeum-logo.svg',
+      rpcs: ['wss://dapps.shardeum.org', 'https://dapps.shardeum.org'],
+      group: GroupChain.Shardeum,
+      getAddress: toEvmAddress,
+    })
+  }
+}
