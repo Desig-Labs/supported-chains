df54baf07e281f7e43b0b0edb3dfb35b6c3f9b84
minhcong <83624415+dmcong@users.noreply.github.com>
2023-08-18 15:59:07 +0700

    supported chains-arb-taiko v1.0.26

--- src/list/arbitrum-mainnet.ts
+++ src/list/arbitrum-mainnet.ts
@@ -12,7 +12,10 @@ export class ArbitrumMainnet extends Chain {
       curve: Curve.secp256k1,
       icon: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/arb-icon.svg',
       logo: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/arb-logo.svg',
-      rpcs: ['https://arb1.arbitrum.io/rpc'],
+      rpcs: [
+        'wss://rpc.ankr.com/arbitrum/ws/6fd209c7de745965c5b2092cb14245501fb6d4bd12a11db785dda62ab94cb2a4',
+        'https://rpc.ankr.com/arbitrum/6fd209c7de745965c5b2092cb14245501fb6d4bd12a11db785dda62ab94cb2a4',
+      ],
       group: GroupChain.Arbitrum,
       getAddress: toEvmAddress,
     })
