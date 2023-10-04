7e04a0b017c0ce4deda1913d7c3c1b5dc53143ba
quanhoang882002 <135295387+hnquan8@users.noreply.github.com>
2023-10-04 17:47:01 +0700

    fix polygon testnet

--- src/list/polygon-pos-testnet.ts
+++ src/list/polygon-pos-testnet.ts
@@ -10,8 +10,8 @@ export class PolygonPoSTestnet extends Chain {
       alias: 'Polygon PoS Testnet',
       chainId: '0x13881',
       curve: Curve.secp256k1,
-      icon: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/polygon-icon.svg',
-      logo: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/polygon-logo.svg',
+      icon: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/polygon-pos-icon.svg',
+      logo: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/polygon-pos-logo.svg',
       rpcs: [
         'wss://rpc.ankr.com/polygon_mumbai/ws/6fd209c7de745965c5b2092cb14245501fb6d4bd12a11db785dda62ab94cb2a4',
         'https://rpc.ankr.com/polygon_mumbai/6fd209c7de745965c5b2092cb14245501fb6d4bd12a11db785dda62ab94cb2a4',
