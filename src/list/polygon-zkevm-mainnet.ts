efc53b36bd8aec82b2bad5e85df64f3e89dc115e
quanhoang882002 <135295387+hnquan8@users.noreply.github.com>
2023-10-04 17:24:23 +0700

    implement polygon pos

--- src/list/polygon-zkevm-mainnet.ts
+++ src/list/polygon-zkevm-mainnet.ts
@@ -10,8 +10,8 @@ export class PolygonZkEVMMainNet extends Chain {
       alias: 'Polygon zkEVM',
       chainId: '0x44d',
       curve: Curve.secp256k1,
-      icon: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/polygon-icon.svg',
-      logo: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/polygon-logo.svg',
+      icon: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/polygon-zkevm-icon.svg',
+      logo: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/polygon-zkevm-logo.svg',
       rpcs: [
         'wss://rpc.ankr.com/polygon_zkevm/ws/6fd209c7de745965c5b2092cb14245501fb6d4bd12a11db785dda62ab94cb2a4',
         'https://rpc.ankr.com/polygon_zkevm/6fd209c7de745965c5b2092cb14245501fb6d4bd12a11db785dda62ab94cb2a4',
