addf2166fda0037c0b5e9935092e134215698651
quanhoang882002 <135295387+hnquan8@users.noreply.github.com>
2023-11-02 11:04:48 +0700

    change brand tomo -> viction

--- src/list/tomo-testnet.ts
+++ src/list/tomo-testnet.ts
@@ -6,8 +6,8 @@ import { toEvmAddress } from '../utils'
 export class TomoTestnet extends Chain {
   constructor() {
     super({
-      name: 'TomoChain Testnet',
-      alias: 'TomoChain Testnet',
+      name: 'Viction Testnet',
+      alias: 'Viction Testnet',
       chainId: '0x59',
       curve: Curve.secp256k1,
       icon: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/tomo-icon.svg',
