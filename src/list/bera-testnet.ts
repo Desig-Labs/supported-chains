1848a66c699fba6878e792f35792c7782c8c306b
thiennv-qng <81727176+thiennv-qng@users.noreply.github.com>
2024-01-25 14:53:25 +0700

    update version

--- src/list/bera-testnet.ts
+++ src/list/bera-testnet.ts
@@ -12,12 +12,16 @@ export class BeraTestnet extends Chain {
       curve: Curve.secp256k1,
       icon: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/bera-icon.svg',
       logo: 'https://raw.githubusercontent.com/Desig-Labs/supported-chains/master/src/static/bera-logo.png',
-      rpcs: ['', 'https://rpc.ankr.com/berachain_testnet'],
+      rpcs: [
+        '',
+        'https://rpc.ankr.com/berachain_testnet/51585d3e88bdfa7a4f9376afeb1b80ecfaf3c9e8edbc80645f251623f8a76e7f',
+      ],
       group: GroupChain.Bera,
       getAddress: toEvmAddress,
       isMainnet: false,
       endpoint: {
-        https: 'https://rpc.ankr.com/berachain_testnet',
+        https:
+          'https://rpc.ankr.com/berachain_testnet/51585d3e88bdfa7a4f9376afeb1b80ecfaf3c9e8edbc80645f251623f8a76e7f',
         wss: '',
       },
     })
